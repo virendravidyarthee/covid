68e1b4eff0ebe544ff65730c08b85eeb7e378a8e
Virendra <virendravidyarthee@gmail.com>
2020-03-25 22:15:59 -0400

    Copy fixes

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,11 +4,11 @@ export const totalProjections =
   'Total confirmed cases projections over the coming week: (±70 cases)';
 export const howItWorks = 'How it works';
 export const explanation =
-  "The past week's data is analysed to determine the growth rate of total confirmed" +
+  "The past week's data is analysed to determine the growth rate of total confirmed " +
   "cases. A mean of that growth rate is then used to project the coming week's outcome";
 export const growthRateSource = 'Source: https://en.wikipedia.org/wiki/Exponential_growth';
 export const disclaimerTitle = ' Disclaimer';
 export const disclaimerBody =
-  'The developer holds no responsibility of the data displayed on this website. This data' +
-  'is merely speculative and is just a projection based on the current trend of change.' +
+  'The developer holds no responsibility of the data displayed on this website. This data ' +
+  'is merely speculative and is just a projection based on the current trend of change. ' +
   'For educational puposes only. Do your part. STAY HOME.';
