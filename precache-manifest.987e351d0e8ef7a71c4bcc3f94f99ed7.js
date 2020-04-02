00626d202533b225a2cad29de4863a95a6270b6c
Virendra <virendravidyarthee@gmail.com>
2020-04-02 12:40:09 -0400

    Updates

--- precache-manifest.987e351d0e8ef7a71c4bcc3f94f99ed7.js
+++ precache-manifest.987e351d0e8ef7a71c4bcc3f94f99ed7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "beb07c8f837d645b2822e4fdf2fe7c61",
+    "revision": "5e1647de9fa332466d71ea1927af46b9",
     "url": "/covid/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid/static/css/2.5a2f7233.chunk.css"
   },
   {
-    "revision": "d348c5eefe47c74062cc",
+    "revision": "16ce7c709d3628709a40",
     "url": "/covid/static/css/main.9df2366f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid/static/js/2.a29354a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d348c5eefe47c74062cc",
-    "url": "/covid/static/js/main.e7c47876.chunk.js"
+    "revision": "16ce7c709d3628709a40",
+    "url": "/covid/static/js/main.0b1a264e.chunk.js"
   },
   {
     "revision": "1d9e34a2345255329d39",
