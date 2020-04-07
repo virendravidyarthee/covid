d16d381aec24d9446d48201e87bd1b26b1fa5661
Virendra <virendravidyarthee@gmail.com>
2020-04-06 20:28:29 -0400

    Updates

--- precache-manifest.3c92b186344bae9e8f317053e3373a9c.js
+++ precache-manifest.3c92b186344bae9e8f317053e3373a9c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad673201d217ac5e6f751c476e8da148",
+    "revision": "f6d254095a11815cb6a4b20089b624e1",
     "url": "/covid/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid/static/css/2.5a2f7233.chunk.css"
   },
   {
-    "revision": "899b3cd49264a395e61d",
+    "revision": "6c7c401fe5f786d4f08c",
     "url": "/covid/static/css/main.9df2366f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid/static/js/2.998e4e74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "899b3cd49264a395e61d",
-    "url": "/covid/static/js/main.87ba48ac.chunk.js"
+    "revision": "6c7c401fe5f786d4f08c",
+    "url": "/covid/static/js/main.50b5ae02.chunk.js"
   },
   {
     "revision": "1d9e34a2345255329d39",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid/precache-manifest.fb0a106ff032240fd2bc39256d91ab33.js"
+  "/covid/precache-manifest.3c92b186344bae9e8f317053e3373a9c.js"
 );
 
 self.addEventListener('message', (event) => {
